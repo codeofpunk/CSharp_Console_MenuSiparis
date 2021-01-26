57cc043368fabc3203282b9c35df3e0fe81057c7
Batuhan <61819067+codeofpunk@users.noreply.github.com>
2021-01-26 21:26:38 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -19,51 +19,63 @@ namespace CSharp_Console_MenuSiparis
 
             string input3 = Console.ReadLine();
             int urunkodu = Convert.ToInt32(input3);
-            string onaykodu = "y";
+            string onaykodu = "x";
 
             switch (urunkodu)
             {
                 case 1:
                     Console.WriteLine(urun1 + " seçtiniz.");
                     Console.WriteLine("Fiyatı: 15TL. Sipariş vermek istiyor musunuz? (y)");
-                    Console.ReadLine();
+                    onaykodu = Console.ReadLine();
                     switch (onaykodu)
                     {
                         case "y":
                             Console.WriteLine(urun1 + " Siparişiniz başarıyla verildi. Ortalama teslim süresi: 10dk.");
+                            break;
+                        default:
+                            Console.WriteLine("Hatalı işlem!");
                             break;
                     }
                     break;
                 case 2:
                     Console.WriteLine(urun2 + " seçtiniz.");
                     Console.WriteLine("Fiyatı: 8TL. Sipariş vermek istiyor musunuz? (y)");
-                    Console.ReadLine();
+                    onaykodu = Console.ReadLine();
                     switch (onaykodu)
                     {
                         case "y":
                             Console.WriteLine(urun2 + " Siparişiniz başarıyla verildi. Ortalama teslim süresi: 4dk.");
+                            break;
+                        default:
+                            Console.WriteLine("Hatalı işlem!");
                             break;
                     }
                     break;
                 case 3:
                     Console.WriteLine(urun3 + " seçtiniz.");
                     Console.WriteLine("Fiyatı: 4TL. Sipariş vermek istiyor musunuz? (y)");
-                    Console.ReadLine();
+                    onaykodu = Console.ReadLine();
                     switch (onaykodu)
                     {
                         case "y":
                             Console.WriteLine(urun3 + " Siparişiniz başarıyla verildi. Ortalama teslim süresi: 2dk.");
+                            break;
+                        default:
+                            Console.WriteLine("Hatalı işlem!");
                             break;
                     }
                     break;
                 case 4:
                     Console.WriteLine(urun4 + " seçtiniz.");
                     Console.WriteLine("Fiyatı: 5TL. Sipariş vermek istiyor musunuz? (y)");
-                    Console.ReadLine();
+                    onaykodu = Console.ReadLine();
                     switch (onaykodu)
                     {
                         case "y":
                             Console.WriteLine(urun4 + " Siparişiniz başarıyla verildi. Ortalama teslim süresi: 1dk.");
+                            break;
+                        default:
+                            Console.WriteLine("Hatalı işlem!");
                             break;
                     }
                     break;
